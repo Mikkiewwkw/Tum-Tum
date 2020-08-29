6dd6f19fb6c6a7866821392e09282354c8477317
Guipengtt <xgp980303@gmail.com>
2020-08-29 18:52:23 -0500

    Update Login page

---
+++ tumtum/src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="Header">
+        <slot name="left"></slot>
+        <slot name="center"></slot>
+        <slot name="right"></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Header"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ tumtum/src/views/Login.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="login">
+        <div class="header-content">
+            Welcome To TUMTUM
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Login",
+    }
+</script>
+
+<style scoped>
+    @import "../assets/css/public.css";
+    .header-content {
+        font-family:  "Bakso Sapi";
+        font-style: normal;
+    }
+
+</style>

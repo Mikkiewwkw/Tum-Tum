fe60ecbf4e7680dbf60b8b4581a29527dde3889f
Guipengtt <xgp980303@gmail.com>
2020-08-31 01:19:42 -0500

    Update Toast

---
+++ tumtum/src/components/common/toast/Toast.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="toast" >
+        <slot name="the-toast" v-if="isShow"></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Toast",
+        props: {
+            isShow: {
+              type: Boolean,
+              default() {
+                  return false;
+              }
+          }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
--- tumtum/src/views/Login/childComponts/CreateCount.vue
+++ tumtum/src/views/Login/childComponts/CreateCount.vue
@@ -2,21 +2,49 @@
     <div>
         CreateCount
         <button @click="btnClick">按钮</button>
+        <toast :isShow="isShow">
+            <toast-item class="toast-item" slot="the-toast" @toastback="toastback"></toast-item>
+        </toast>
     </div>
 </template>
 
 <script>
+    import Toast from "@/components/common/toast/Toast";
+    import ToastItem from "./toastItem/ToastItem";
     export default {
         name: "CreateCount",
+        components: {
+            Toast,
+            ToastItem,
+        },
+        data() {
+            return {
+                isShow: false,
+                isBack: false,
+            }
+        },
         methods: {
             btnClick() {
                 // console.log("Hello");
-                this.$router.push("/intro");
+                // this.$router.push("/intro");
+                this.isShow = true;
+                if (this.isBack == this.isShow) {
+                    this.isShow = false;
+                    this.$router.push("/intro");
+                }
+            },
+            toastback() {
+                this.isBack = true;
+                this.isShow = false;
+                console.log("GO BACK !!!");
             }
         }
     }
 </script>
 
 <style scoped>
-
+    .toast-item {
+        position: absolute;
+        left: 7%;
+    }
 </style>

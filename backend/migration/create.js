35a813b84d8174a39444faa1f2d9f0dfb9da9f98
wuyumk7 <hymeldon@gmail.com>
2020-09-08 17:40:17 -0500

    (backend:migration) Initialize data tables with different entries

--- backend/migration/create.js
+++ backend/migration/create.js
@@ -3,22 +3,15 @@
 const model = require('../libs/model'),
       config = require('../configs');
 
-const bcrypt = require('bcrypt');
+const init = require('./initialization');
 
 (async () => {
   try {
     await model.db.sequelize.authenticate();
     model.db.sequelize.sync();
   
-    let User = model.User;
-    let salt = await bcrypt.genSalt(10),
-	pwd = await bcrypt.hash('testpassword', salt + config.site.hashSalt);
-    let first_user = User.build({
-      username: 'testuser',
-      salt: salt,
-      password: pwd,
-    });  
-    await first_user.save();
+    await init.buddy(model);
+    await init.user(model, config);
   } catch (err) {
     console.log(err);
   }
---
+++ backend/migration/initialization/index.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const user = require('./user'),
+      buddy = require('./buddy');
+
+module.exports = {
+  user: user,
+  buddy: buddy,
+};

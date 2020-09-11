dd89bba2d4ccf419c4171060bb10c98ba4e3a478
wuyumk7 <hymeldon@gmail.com>
2020-09-10 23:04:58 -0500

    (backend: routers/migration) Update site/signin and test user data

--- backend/migration/initialization/user.js
+++ backend/migration/initialization/user.js
@@ -15,7 +15,7 @@ module.exports = (async (model, config) => {
       lastname: 'User',
       nickname: 'testuser',
       email: 'testuser@tumtum.com',
-      //phone: '1654230',
+      phone: '16542309181',
       password: pwd,
     });  
     first_user.setBuddy(buddy, { save: false });

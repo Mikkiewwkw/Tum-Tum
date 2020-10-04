0693334a1c28c44b0c617058a549b5d63c23f0db
wuyumk7 <hymeldon@gmail.com>
2020-10-03 19:22:07 -0500

    (backend: libs) Use twilio as the default SMS provider

--- backend/libs/sms.js
+++ backend/libs/sms.js
@@ -1,20 +1,24 @@
 'use strict'
 
-const axios = require('axios');
+const axios = require('axios'),
+      Twilio = require('twilio');
 
 const serviceConfig = require('../configs').service;
 
 module.exports.sendSMS = ((config) => {
-  return async (SMS, service="rapid") => {
+  return async (SMS, service="twilio") => {
     let error = new Error("Cannot reach the target phone number.");
     error.name = "SendSMSFailedError";
+
     try {
-      const SMSConfig = config.sendSMS;
+      let SMSConfig = config.sendSMS;
       let res;
+      let sendSucc = false;
 
-      if (service == "rapidxxx") {
+      if (service == "xxx") {
 
-      } else {
+      } else if (service == "rapid") {
+        SMSConfig = SMSConfig[service];
         const rapidSMSReq = {
           "method": "POST",
           "url": SMSConfig.url,
@@ -34,16 +38,32 @@ module.exports.sendSMS = ((config) => {
         };
         res = await axios(rapidSMSReq);
         if (res.data && res.data.data && res.data.data.startsWith("Success")) {
-          return "success";
-	} else {
+          sendSucc = true;
+        } else {
           if (res.data && res.data.data)
             error.message = res.data.data;
-	  throw error;
-	}
+        }
         //console.log(res);
+      } else {
+        SMSConfig = SMSConfig['twilio'];
+        // TODO: add webhooks to track message status
+        const accountSid = SMSConfig.accountSid,
+              authToken = SMSConfig.authToken;
+        const client = Twilio(accountSid, authToken);
+	
+        res = await client.messages.create({
+                body: SMS.content,
+                messagingServiceSid: SMSConfig.msgServiceId,
+                to: SMS.to
+              });
+        return res.sid;
       }
       //console.log(res.data);
-      return "fail";
+      if (sendSucc == true) {
+        return "success";
+      } else {
+        throw error;
+      }
     } catch (err) {
       console.log(err); 
       throw err;
